34b7cf85cd67bfecd3cdf58815556cde044b2fc1
AnitaAhmeti1 <anita.ahmeti2@student.uni-pr.edu>
2025-11-09 20:32:44 +0100

    firebase

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCi6Kx4v8DWAS5DsMCf29YfPrrUYoiDM-A",
+  authDomain: "healthbuddy-545ba.firebaseapp.com",
+  projectId: "healthbuddy-545ba",
+  storageBucket: "healthbuddy-545ba.firebasestorage.app",
+  messagingSenderId: "288329939493",
+  appId: "1:288329939493:web:f6f0ee94a7b31a07901dcc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
