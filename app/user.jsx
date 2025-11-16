e906e1fc83dbe39395dfe59fcb716a4749dcf769
ArxhentëKadriu <149197219+arxhentakadriu@users.noreply.github.com>
2025-11-16 15:16:10 +0100

    Create user.jsx

---
+++ app/user.jsx
@@ -0,0 +1,78 @@
+import { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  ActivityIndicator,
+  FlatList,
+  StyleSheet,
+} from "react-native";
+
+export default function Users() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const loadUsers = async () => {
+      try {
+        const res = await fetch("https://jsonplaceholder.typicode.com/users");
+        const data = await res.json();
+        setUsers(data);
+      } catch (error) {
+        console.log("API Error:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    loadUsers();
+  }, []);
+
+  if (loading) {
+    return (
+      <View style={styles.center}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>External API – JSONPlaceholder Users</Text>
+
+      <FlatList
+        data={users}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View style={styles.card}>
+            <Text style={styles.name}>{item.name}</Text>
+            <Text>{item.email}</Text>
+          </View>
+        )}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { padding: 20 },
+  center: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  card: {
+    padding: 15,
+    backgroundColor: "#eee",
+    marginBottom: 10,
+    borderRadius: 10,
+  },
+  name: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+});
